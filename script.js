5f131c0f56869a28e2462dd29123115e4075107e
Alexandr Kotenko <kpshmt@gmail.com>
2024-05-30 11:22:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
 document.addEventListener("DOMContentLoaded", function() {
     const updateDateElement = document.getElementById("update-date");
 
-    // Задание начальной даты
-    const initialDate = new Date(2024, 4, 10); // Месяцы в JavaScript считаются с 0, поэтому май - это 4
+    // Установка начальной даты
+    const initialDate = new Date(2024, 4, 20); // Месяцы в JavaScript считаются с 0, поэтому май - это 4
 
-    function getNextUpdateDate(startDate) {
+    // Логика для добавления 5 дней к заданной дате
+    const getNextUpdateDate = (startDate) => {
         const nextUpdateDate = new Date(startDate);
         nextUpdateDate.setDate(startDate.getDate() + 5); // Обновляем каждые 5 дней
         return nextUpdateDate;
-    }
+    };
 
-    function formatDate(date) {
-        const day = date.getDate().toString().padStart(2, '0');
-        const month = (date.getMonth() + 1).toString().padStart(2, '0');
-        const year = date.getFullYear();
-        return `${day}.${month}.${year}`;
-    }
-
-    function updateDate() {
+    const updateDate = () => {
         const today = new Date();
         let lastUpdatedDate = new Date(initialDate);
 
         // Найти последнюю дату обновления, которая была раньше или равна сегодняшней дате
-        while ((today - lastUpdatedDate) >= 5 * 24 * 60 * 60 * 1000) {
+        while (lastUpdatedDate <= today) {
             lastUpdatedDate = getNextUpdateDate(lastUpdatedDate);
         }
 
-        updateDateElement.textContent = `Updated: ${formatDate(lastUpdatedDate)}`;
-    }
+        // Назначаем предыдущую дату обновления, которая будет меньше или равна сегодняшней
+        lastUpdatedDate.setDate(lastUpdatedDate.getDate() - 5);
 
-    updateDate();
+        updateDateElement.textContent = `Updated: ${formatDate(lastUpdatedDate)}`;
+    };
+
+    const formatDate = (date) => {
+        const day = date.getDate().toString().padStart(2, '0');
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const year = date.getFullYear();
+        return `${day}.${month}.${year}`;
+    };
+
+    // Устанавливаем дату 25.05.2024 для отображения
+    const displayDate = new Date(2024, 4, 25); // Месяцы в JavaScript считаются с 0, поэтому май - это 4
+    updateDateElement.textContent = `Updated: ${formatDate(displayDate)}`;
+
+    // Проверка обновления каждые час
+    setInterval(updateDate, 60 * 60 * 1000);
 });
